60a2da817f99c9685e7d9e6c67157c4d0b93dc35
Pranav Vashi <neobuddy89@gmail.com>
2019-07-07 11:56:32 +0530

    Fix long click intent for Smart Pixels tile [2/2]
    Change-Id: Id6b5fffd814f035be1bb75d1434456e0c02aa97f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -587,4 +587,6 @@ public class Settings extends SettingsActivity {
 
     /** Evolution X additions */
     public static class PrivateDnsSettingsActivity extends SettingsActivity { /* empty */ }
+
+    public static class SmartPixelsActivity extends SettingsActivity {}
 }
--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -220,6 +220,7 @@ import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSetting
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import org.evolution.settings.EvolutionSettings;
+import org.evolution.settings.fragments.miscellaneous.SmartPixels;
 
 public class SettingsGateway {
 
@@ -429,6 +430,7 @@ public class SettingsGateway {
             AppLocalePickerFragment.class.getName(),
             SupervisionDashboardFragment.class.getName(),
             PrivateDnsSettings.class.getName(),
+            SmartPixels.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -479,5 +481,6 @@ public class SettingsGateway {
             Settings.ShadeSettingsActivity.class.getName(),
             // Evolution X additions
             Settings.PrivateDnsSettingsActivity.class.getName(),
+            Settings.SmartPixelsActivity.class.getName(),
     };
 }
