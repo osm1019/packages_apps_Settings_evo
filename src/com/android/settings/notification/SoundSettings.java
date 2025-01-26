8408444d98d566f54a17ac76db73584f8612f04a
Joey Huab <jhuab@edukasyon.ph>
2025-01-27 05:01:30 +0900

    Settings: Do not hide Now Playing for Pixel devices

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -57,6 +57,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.evolution.settings.utils.DeviceUtils;
+
 @SearchIndexable
 public class SoundSettings extends DashboardFragment implements OnActivityResultListener {
     private static final String TAG = "SoundSettings";
@@ -126,7 +128,9 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
             }
             return null;
         });
-        updateAmbientMusicPref();
+        if (!DeviceUtils.isCurrentlySupportedPixel()) {
+            updateAmbientMusicPref();
+        }
     }
 
     private void updateAmbientMusicPref() {
